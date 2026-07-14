e8f1eeef69ed644dc8fa17bc80d0280e7e7a62ad
Jaheer1088 <jaheer1088@gmail.com>
2026-07-14 09:40:52 +0530

    Enhance Playwright test workflow with retry logic and increase worker count for CI

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,8 +3,26 @@ import {test} from '../fixtures/fixtures'
 
 
 test('has title', async ({ page, loginPage }) => {
-  await loginPage.login("10888@gmail.com", "12345");
-  console.log("DONE");
+  
+  function duplicateChar(input:string){
+    let map = new Map<string,number>();
+    input.split("").forEach(ele =>{
+        if(!map.has(ele)){
+           map.set(ele,(map.get(ele)||0)+1); 
+        }else{
+          map.set(ele,(map.get(ele)||0)+1); 
+        }
+    })
+    console.log(map)
+    for(let [key,value] of map){
+        if(value>1){
+          console.log("duplicate chars are :" + key)
+        }
+    }
+}
+
+duplicateChar("QAQA");
+
 });
 
 
